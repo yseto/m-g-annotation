0d999cbc72a971641b2e3d385dd09c74b208c746
yseto <yseto@users.noreply.github.com>
2022-04-22 13:12:17 +0000

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+
+async function run() {
+  try {
+    core.saveState("startEpoch", Math.round(Date.now() / 1000));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
+
